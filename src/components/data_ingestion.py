18084428c146d18a6db34b32ce4782cd23b45e2b
Snehojit Banerjee <snehojit2@gmail.com>
2026-09-08 13:08:57 +0530

    Hyperparameter Training

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -75,6 +75,6 @@ if __name__ == "__main__":
     model_trainer = ModelTrainer()
     print(model_trainer.initiate_model_trainer(train_arr, test_arr))
 
-    print("Model Trainer")
+    print("Model Training done.")
                
 
